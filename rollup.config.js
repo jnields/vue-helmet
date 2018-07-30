7b5bfd3684175d396fd63889475e03b46582e148
Joe Nields <jnields@rei.com>
2018-07-30 13:59:45 -0700

    Add sourcemaps

--- rollup.config.js
+++ rollup.config.js
@@ -7,10 +7,12 @@ export default {
     {
       file: 'dist/index.js',
       format: 'cjs',
+      sourcemap: true,
     },
     {
       file: 'dist/index.mjs',
       format: 'es',
+      sourcemap: true,
     },
   ],
   plugins: [
